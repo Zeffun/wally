b2a6a9a1122e709c97df9def7b04e1c9c46820c5
MH S <gopperlie123@gmail.com>
2024-08-30 11:06:52 +0800

    chore: schema update

--- backend/models/Accounts.js
+++ backend/models/Accounts.js
@@ -1,29 +1,32 @@
 const mongoose = require("mongoose");
 
-const accountSchema = new mongoose.Schema({
-  acId: {
-    //this is the account number, we generate our own 12-digit unique number
-    type: Number,
-    required: true,
+const accountSchema = new mongoose.Schema(
+  {
+    acId: {
+      //this is the account number, we generate our own 12-digit unique number
+      type: Number,
+      required: true,
+    },
+    dateCreated: {
+      //{timestamp}
+      type: Date,
+      required: true,
+    },
+    userid: {
+      //connect via obj id-s
+      type: Number,
+      required: true,
+    },
+    currency: {
+      type: String,
+      required: true,
+    },
+    balance: {
+      type: Number,
+      required: true,
+    },
   },
-  dateCreated: {
-    //{timestamp}
-    type: Date,
-    required: true,
-  },
-  userid: {
-    //connect via obj id-s
-    type: Number,
-    required: true,
-  },
-  currency: {
-    type: String,
-    required: true,
-  },
-  balance: {
-    type: Number,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Account", accountSchema);
--- backend/models/Transactions.js
+++ backend/models/Transactions.js
@@ -1,32 +1,31 @@
 const mongoose = require("mongoose");
 
-const transactionSchema = new mongoose.Schema({
-  date: {
-    //{timestamp}
-    type: Date, //needs to conform to the ISO 8601 format. will already include the time
+const transactionSchema = new mongoose.Schema(
+  {
+    senderAcc: {
+      //referencing
+      type: Number,
+      required: true,
+    },
+    receiverAcc: {
+      //referencing
+      type: Number,
+      required: true,
+    },
+    currency: {
+      type: string,
+      required: true,
+    },
+    amount: {
+      //validation, should not allow -ve values
+      type: Number,
+      required: true,
+    },
+    purpose: {
+      type: String,
+    },
   },
-  senderAcc: {
-    //referencing
-    type: Number,
-    required: true,
-  },
-  receiverAcc: {
-    //referencing
-    type: Number,
-    required: true,
-  },
-  currency: {
-    type: string,
-    required: true,
-  },
-  amount: {
-    //validation, should not allow -ve values
-    type: Number,
-    required: true,
-  },
-  purpose: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Transaction", transactionSchema);
