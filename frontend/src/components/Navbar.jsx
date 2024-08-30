1983627eb30f6f2b22c299033e31c9b90b344993
Aloy <aloyleow@gmail.com>
2024-08-30 23:51:08 +0800

    feat: navBar skeleton complete

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,32 +2,71 @@ import { useState } from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
+import { useNavigate } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#01579b',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#01579b',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
 
 
 export default function Navbar(){
 
-
-  const [value, setValue] = useState('one');
+  const [page, setPage] = useState("home");
+  const navigate = useNavigate()
 
   const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+
+    setPage(newValue)
+
+    if (newValue === "home") {
+      navigate("/")
+    } else if (newValue === "signIn") {
+      navigate("/signin")
+    } else if (newValue === "signUp") {
+      navigate("/signup")
+    } else if (newValue === "about") {
+      navigate("/about")
+    } else if (newValue === "facts") {
+      navigate("/facts")
+    }
+  }
+
   
-  
+
   return (
-    <Box sx={{ width: '100%' }}>
-    <Tabs
-      value={value}
-      onChange={handleChange}
-      textColor="secondary"
-      indicatorColor="secondary"
-      aria-label="secondary tabs example"
-    >
-      <Tab value="one" label="Item One" />
-      <Tab value="two" label="Item Two" />
-      <Tab value="three" label="Item Three" />
-    </Tabs>
-  </Box>
+    <ThemeProvider theme={theme}>
+    <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center'}}>
+      <Tabs
+        value={page}
+        onChange={handleChange}
+        textColor="secondary"
+        indicatorColor="secondary"
+        aria-label="homePageMenu"
+        variant="scrollable"
+        scrollButtons="auto"
+      >
+        <Tab sx={{ mx: 2 }} value="home" label="Home" />
+        <Tab sx={{ mx: 2 }} value="signIn" label="Sign In" />
+        <Tab sx={{ mx: 2 }} value="signUp" label="Sign Up" />
+        <Tab sx={{ mx: 2 }} value="about" label="About" />
+        <Tab sx={{ mx: 2 }} value="facts" label="FAQ" />
+      </Tabs>
+    </Box>
+    </ThemeProvider>
   )
 }
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,1 +1,1 @@
-export default function HomePage(){return(<></>)}
+export default function HomePage(){return(<><h1>home</h1></>)}
