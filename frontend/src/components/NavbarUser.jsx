c4bef0e6d85a8404b840e0d102eba21151d7b74d
Aloy <aloyleow@gmail.com>
2024-08-31 16:24:03 +0800

    chore: working on AMpage

--- frontend/src/components/NavbarUser.jsx
+++ frontend/src/components/NavbarUser.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -17,8 +17,8 @@ const pages = ['Dashboard', 'Transactions', 'Payments', 'Deposits'];
 const settings = ['Profile', 'Account', 'Help', 'Logout'];
 
 export default function NavbarUser() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
