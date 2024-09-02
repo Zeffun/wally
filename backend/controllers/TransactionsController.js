2b0aaebc627f4c785a4ab391de6986d7451002e7
MH S <gopperlie123@gmail.com>
2024-09-02 09:20:28 +0800

    test: controller

--- backend/controllers/TransactionsController.js
+++ backend/controllers/TransactionsController.js
@@ -1,46 +1,35 @@
 const express = require("express");
-const Transaction = require("../models/Transaction");
 const router = express.Router();
-const verifyToken = require("../middleware/verify-token");
+const Transaction = require("../models/Transaction");
 const debug = require("debug")("wally:transaction:TransactionsController");
+// const verifyToken = require("../middleware/verify-token");
 
 //protected routes
 
 // router.use(verifyToken);
-const mockAccount1 = {
-  _id: 12345667,
-  acId: 123456789101,
-};
 
-const ac1Num = mockAccount1.acId;
-
-const mockAccount2 = {
-  acId: 123456789102,
-};
-
-const mockTransaction = {
-  senderAcc: mockAccount1.acId,
-  receiverAcc: mockAccount2.acId,
-  currency: "SGD",
-  amount: 1000,
-  purpose: "test",
-};
+// const mockAccount1 = {
+//   _id: 12345667,
+//   acId: 123456789101,
+// };
+// const mockAccount2 = {
+//   acId: 123456789102,
+// };
 
 // router.use(verifyToken);
 
-router.post("/new", (req, res) => {
-  res.status(200).send("Route is working");
-  // debug(`body: %o`, req.body);
-  // console.log(`body:%o`, req.body);
-  // try {
-  //   req.body.userid = mockAccount1._id; // Corrected the variable name
-  //   const transaction = await Transaction.create(req.body);
-  //   transaction._doc.userid = mockAccount1._id; // Assuming currentUser is meant to be mockAccount1
-  //   res.status(201).json(transaction);
-  // } catch (error) {
-  //   console.error("Error during transaction creation:", error); // More detailed logging
-  //   res.status(500).json({ error: error.message }); // Provide error message in response
-  // }
+router.post("/new", async (req, res) => {
+  // res.status(201).send("Route is working");
+  debug(`body: %o`, req.body);
+  try {
+    req.body.userid = mockAccount1._id; // Corrected the variable name
+    const transaction = await Transaction.create(req.body);
+    transaction._doc.userid = mockAccount1._id; // Assuming currentUser is meant to be mockAccount1
+    res.status(201).json(transaction);
+  } catch (error) {
+    console.error("Error during transaction creation:", error); // More detailed logging
+    res.status(500).json({ error: error.message }); // Provide error message in response
+  }
 });
 
 // Transaction.find({ userAcc: ac1Num })
@@ -54,3 +43,4 @@ router.post("/new", (req, res) => {
 //   });
 
 module.exports = router;
+console.log("TransactionsController loaded");
