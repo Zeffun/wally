3bd5c00369860f4a0aee82ebdf4f625bda8fec9c
MH S <gopperlie123@gmail.com>
2024-09-03 22:05:21 +0800

    feat: loading for transfers

--- frontend/src/pages/AccountTransfersPage.jsx
+++ frontend/src/pages/AccountTransfersPage.jsx
@@ -2,10 +2,12 @@ import { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 import { newTransfer } from "../services/transfService";
 import { getAccounts } from "../services/authService";
-import Button from "@mui/material/Button";
 // import Stack from "@mui/material/Stack";
 
 const currencies = [
@@ -15,7 +17,7 @@ const currencies = [
   },
 ];
 export default function AccountTransfersPage() {
-  // const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [accounts, setAccounts] = useState([]);
   const [error, setError] = useState(null);
   const [transferData, setTransferData] = useState({
@@ -50,7 +52,7 @@ export default function AccountTransfersPage() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(transferData);
+    setIsLoading(true);
     try {
       const amount = parseFloat(transferData.amount);
       const newTransferResponse = await newTransfer({
@@ -61,6 +63,8 @@ export default function AccountTransfersPage() {
       navigate("/account/main");
     } catch (err) {
       console.error(err.message);
+    } finally {
+      setIsLoading(false);
     }
   };
   useEffect(() => {
@@ -72,121 +76,130 @@ export default function AccountTransfersPage() {
   }, []);
 
   return (
-    <Box
-      component="form"
-      sx={{
-        "& .MuiTextField-root": { m: 1.5, width: "35ch" },
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center", // Center vertically
-        alignItems: "center", // Center horizontally
-        height: "100vh",
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center", // Align the text fields and labels to the left
-        }}
+    <>
+      <Backdrop
+        sx={(theme) => ({ color: "#fff", zIndex: theme.zIndex.drawer + 1 })}
+        open={isLoading}
       >
-        <Box sx={{ marginRight: 2 }}>Send it to : </Box>{" "}
-        <TextField
-          id="controlled acnum"
-          label="Receipient account no."
-          name="receiverAcc"
-          value={transferData.receiverAcc}
-          onChange={handleChange}
-        />
-      </Box>
+        <CircularProgress color="inherit" />
+      </Backdrop>
       <Box
+        component="form"
         sx={{
+          "& .MuiTextField-root": { m: 1.5, width: "35ch" },
           display: "flex",
-          alignItems: "center",
+          flexDirection: "column",
+          justifyContent: "center", // Center vertically
+          alignItems: "center", // Center horizontally
+          height: "100vh",
         }}
+        noValidate
+        autoComplete="off"
       >
-        <Box sx={{ marginRight: 2 }}>Send from:</Box> {/* Label on the left */}
-        <TextField
-          id="senderAcc"
-          select
-          label="Please select sending account"
-          helperText=""
-          name="senderAcc"
-          onChange={handleChange}
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center", // Align the text fields and labels to the left
+          }}
         >
-          {accounts.map((account) => (
-            <MenuItem key={account._id} value={account._id}>
-              {account._id}
-            </MenuItem>
-          ))}
-        </TextField>
-      </Box>
-      <Box
-        sx={{
-          width: "46ch",
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        <Box sx={{ marginRight: 2 }}>Currency</Box> {/* Label on the left */}
-        <TextField
-          id="currency"
-          select
-          label="Select currency"
-          defaultValue="S$"
-          helperText=""
-          value={transferData.currency}
-          name="currency"
-          onChange={handleChange}
+          <Box sx={{ marginRight: 2 }}>Send it to : </Box>{" "}
+          <TextField
+            id="controlled acnum"
+            label="Receipient account no."
+            name="receiverAcc"
+            value={transferData.receiverAcc}
+            onChange={handleChange}
+          />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+          }}
         >
-          {currencies.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-        <Box sx={{ marginRight: 2 }}>Amt:</Box> {/* Label on the left */}
-        <TextField
-          id="amt"
-          label=""
-          value={transferData.amount}
-          name="amount"
-          onChange={handleChangeAmt}
-          error={error}
-          helperText={error}
-        />
+          <Box sx={{ marginRight: 2 }}>Send from:</Box>{" "}
+          {/* Label on the left */}
+          <TextField
+            id="senderAcc"
+            select
+            label="Please select sending account"
+            helperText=""
+            name="senderAcc"
+            onChange={handleChange}
+          >
+            {accounts.map((account) => (
+              <MenuItem key={account._id} value={account._id}>
+                {account._id}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Box>
+        <Box
+          sx={{
+            width: "46ch",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ marginRight: 2 }}>Currency</Box> {/* Label on the left */}
+          <TextField
+            id="currency"
+            select
+            label="Select currency"
+            defaultValue="S$"
+            helperText=""
+            value={transferData.currency}
+            name="currency"
+            onChange={handleChange}
+          >
+            {currencies.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <Box sx={{ marginRight: 2 }}>Amt:</Box> {/* Label on the left */}
+          <TextField
+            id="amt"
+            label=""
+            value={transferData.amount}
+            name="amount"
+            onChange={handleChangeAmt}
+            error={error}
+            helperText={error}
+          />
+        </Box>
+        <Box
+          sx={{
+            width: "46ch",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ marginRight: 2 }}>Purpose:</Box> {/* Label on the left */}
+          <TextField
+            id="Purpose"
+            label="Purpose of transfer"
+            variant="filled"
+            name="purpose"
+            onChange={handleChange}
+          />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end", // Align button to the right
+            width: "100%", // Full width
+            maxWidth: "400px", // Limit the width of the main container
+            mt: 3, // Add some spacing above the button
+          }}
+        >
+          <Button variant="contained" onClick={handleSubmit}>
+            Send now
+          </Button>
+        </Box>
       </Box>
-      <Box
-        sx={{
-          width: "46ch",
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        <Box sx={{ marginRight: 2 }}>Purpose:</Box> {/* Label on the left */}
-        <TextField
-          id="Purpose"
-          label="Purpose of transfer"
-          variant="filled"
-          name="purpose"
-          onChange={handleChange}
-        />
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "flex-end", // Align button to the right
-          width: "100%", // Full width
-          maxWidth: "400px", // Limit the width of the main container
-          mt: 3, // Add some spacing above the button
-        }}
-      >
-        <Button variant="contained" onClick={handleSubmit}>
-          Send now
-        </Button>
-      </Box>
-    </Box>
+    </>
   );
 }
