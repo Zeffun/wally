a5f7d6b50d2ccc7835c07ba624674e394afdeb57
Zeffun <84435005+Zeffun@users.noreply.github.com>
2024-09-05 14:49:48 +0800

    added help page

--- frontend/src/pages/AccountHelpPage.jsx
+++ frontend/src/pages/AccountHelpPage.jsx
@@ -1,5 +1,48 @@
 export default function AccountHelpPage(){
     return (<>
-        <p>This app is pretty self-explanatory</p>
+        <h1>Help Center</h1>
+        <p>Welcome to Wally! Here's how you can easily manage your transactions.</p>
+        <br></br>
+        <h2>Transfers</h2>
+        <p>To transfer money to another account:</p>
+        <ol>
+            <li>1. Go to the Transfers section.</li>
+            <li>2. Enter the recipient's account number.</li>
+            <li>3. Specify the amount you wish to send.</li>
+            <li>4. Confirm your transfer.</li>
+        </ol>
+        <br></br>
+        <h2>Deposits</h2>
+        <p>To deposit funds into your Wally account:</p>
+        <ol>
+            <li>1. Navigate to the Deposit tab.</li>
+            <li>2. Enter the amount you'd like to add.</li>
+            <li>3. Confirm the deposit to update your balance.</li>
+        </ol>
+        <br></br>
+        <h2>Withdrawals</h2>
+        <p>To withdraw money from your account:</p>
+        <ol>
+            <li>1. Select the Withdraw option.</li>
+            <li>2. Enter the amount you want to withdraw.</li>
+            <li>3. Confirm the withdrawal, and the funds will be deducted from your balance.</li>
+        </ol>
+        <br></br>
+        <hr></hr>
+        <br></br>
+        <p><em>If you have any questions or run into any issues, feel free to reach out to our support team. We're here to assist you!</em></p>
     </>)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
