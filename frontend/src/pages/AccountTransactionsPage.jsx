176884bae3492a701d0e863f06e48353132caab6
Aloy <aloyleow@gmail.com>
2024-09-05 18:10:21 +0800

    chore: readjust css on Transfer page just in case

--- frontend/src/pages/AccountTransactionsPage.jsx
+++ frontend/src/pages/AccountTransactionsPage.jsx
@@ -67,8 +67,8 @@ export default function AccountTransactionsPage(props) {
       >
         <br></br>
         <br></br>
-        <Typography sx={{ padding: 2, fontSize: { xs: "1rem", md: "2rem" } }}>
-          <h1>Transfers</h1>
+        <Typography sx={{ padding: 2, fontSize: { xs: "1rem", md: "2rem" }, fontWeight: "bold" }}>
+          Transfers
         </Typography>
         {transactions.map((transaction, index) => (
           <Card key={index} sx={{ mb: 2, width: "100%" }}>
@@ -134,8 +134,8 @@ export default function AccountTransactionsPage(props) {
             </CardContent>
           </Card>
         ))}
-        <Typography sx={{ padding: 2, fontSize: { xs: "1rem", md: "2rem" } }}>
-          <h1>Deposits/Withdrawals</h1>
+        <Typography sx={{ padding: 2, fontSize: { xs: "1rem", md: "2rem" }, fontWeight: "bold" }}>
+          Deposits/Withdrawals
         </Typography>
         {updates.map((update, index) => (
           <Card key={index} sx={{ mb: 2, width: "100%" }}>
@@ -144,7 +144,7 @@ export default function AccountTransactionsPage(props) {
                 ${Math.abs(update.amount)}
               </Typography>
               <CardContent
-                sx={{ display: "flex", justifyContent: "space-between" }}
+                sx={{ display: {xs: "block", md: "flex"}, justifyContent: "space-between" }}
               >
                 <Box>
                   <Typography>
