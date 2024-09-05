2cfde5bfe78477708d19bbee7fbe70b8131ed4be
Aloy <aloyleow@gmail.com>
2024-09-05 20:39:54 +0800

    chore: transfers

--- frontend/src/pages/AccountTransfersPage.jsx
+++ frontend/src/pages/AccountTransfersPage.jsx
@@ -8,9 +8,7 @@ import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 import { newTransfer } from "../services/transfService";
 import { getAccountById, getAccounts } from "../services/authService";
-import { Container, Typography, Paper } from "@mui/material";
 // import Stack from "@mui/material/Stack";
-
 const currencies = [
   {
     value: "SGD",
@@ -37,7 +35,6 @@ export default function AccountTransfersPage() {
   const [accounts, setAccounts] = useState([]);
   const [recName, setRecName] = useState("Receipient account no.");
   const [error, setError] = useState(null);
-  const [fieldError, setFieldError] = useState(null);
   const [transferData, setTransferData] = useState({
     senderAcc: "",
     receiverAcc: "",
@@ -45,24 +42,20 @@ export default function AccountTransfersPage() {
     amount: 0,
     purpose: "",
   });
-
   const navigate = useNavigate();
   const handleChange = (event) => {
     const { name, value } = event.target;
     setTransferData({ ...transferData, [name]: value });
-
-    if (value === "" || value.length <= 36) {
-      setFieldError(null);
-    } else {
-      // Show error if length exceeds 36 characters
-      setFieldError("Please limit to 36 characters or less.");
-    }
   };
-
   const handleChangeAmt = (event) => {
     const amountRegex = /^\d*\.?\d{0,2}$/;
     const value = event.target.value;
-    if (value === "" || amountRegex.test(value)) {
+    if (value === "") {
+      setTransferData({ ...transferData, amount: value });
+      setError(null);
+      return;
+    }
+    if (amountRegex.test(value)) {
       setTransferData({ ...transferData, amount: value });
       setError(null);
     } else {
@@ -74,7 +67,6 @@ export default function AccountTransfersPage() {
     try {
       // Extract receiverAcc from transferData
       const { receiverAcc } = transferData;
-
       // Call getAccountById with receiverAcc as a parameter
       const checkNameResponse = await getAccountById({
         accountId: receiverAcc,
@@ -86,17 +78,16 @@ export default function AccountTransfersPage() {
       setRecName("No account found");
     }
   };
-
   const handleSubmit = async (event) => {
     event.preventDefault();
     setIsLoading(true);
     try {
       const amount = parseFloat(transferData.amount);
-      await newTransfer({
+      const newTransferResponse = await newTransfer({
         ...transferData,
         amount,
       });
-      
+      console.log(newTransferResponse);
       navigate("/account/main");
     } catch (err) {
       console.error(err.message);
@@ -111,7 +102,6 @@ export default function AccountTransfersPage() {
     };
     loadAccount();
   }, []);
-
   return (
     <>
       <Backdrop
@@ -123,72 +113,78 @@ export default function AccountTransfersPage() {
       <Box
         component="form"
         sx={{
-          height: "100vh",
+          "& .MuiTextField-root": { m: 1.5, width: "35ch" }, // TextField margin and width
           display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+          flexDirection: "column",
+          justifyContent: "center", // Center vertically
+          alignItems: "center", // Center horizontally
+          height: "100vh",
+          paddingTop: "64px",
         }}
         noValidate
         autoComplete="off"
       >
-        <Paper elevation={10} sx={{ padding: 3, width: {xs: "90%", md: "30%"}}}>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-start",
-              mb: 1, // Align the text fields and labels to the left
-            }}
-          >
-            <Typography sx={{ marginRight: 2 , fontSize: {xs: "80%", md: "100%"}}}>Send it to : </Typography>{" "}
-            <TextField
-              id="controlled acnum"
-              label={recName}
-              name="receiverAcc"
-              value={transferData.receiverAcc}
-              sx={{width: "70%"}}
-              onChange={handleChange}
-            />
-          </Box>
-          <Box sx={{display: "flex", justifyContent: {xs: "center", md: "flex-end"}, mr: 2.5, mb: 1}}>
-          <Button onClick={handleCheckName}>Check receipient</Button>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-start",
-              mb: 2.5,
-            }}
-          >
-            <Typography sx={{ marginRight: 2, fontSize: {xs: "80%", md: "100%"} }}>Send from:</Typography>{" "}
-            {/* Label on the left */}
-            <TextField
-              id="senderAcc"
-              select
-              label="Please select sending account"
-              helperText=""
-              value={transferData.senderAcc}
-              name="senderAcc"
-              sx={{width: "70%"}}
-              onChange={handleChange}
-            >
-              {accounts.map((account) => (
-                <MenuItem key={account._id} value={account._id}>
-                  {account._id}
-                </MenuItem>
-              ))}
-            </TextField>
-          </Box>
-          <Box
-          sx={{         
+        <Box
+          sx={{
             display: "flex",
             alignItems: "center",
-            justifyContent: "flex-start",
-            mb: 2.5
+            justifyContent: "center", // Align the text fields and labels to the left
           }}
         >
-          <Typography sx={{ marginRight: {xs: 1, md: 3}, fontSize: {xs: "80%", md: "100%"} }}>Currency</Typography> {/* Label on the left */}
+          <Box sx={{ marginRight: 2 }}>Send it to : </Box>{" "}
+          <TextField
+            id="controlled acnum"
+            label={recName}
+            name="receiverAcc"
+            value={transferData.receiverAcc}
+            sx={boxShadowStyle}
+            onChange={handleChange}
+          />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end", // Align button to the right
+            width: "100%", // Full width
+            maxWidth: "400px", // Limit the width of the main container
+            mt: 0.25, // Add some spacing above the button
+          }}
+        >
+          <Button onClick={handleCheckName}>Check receipient</Button>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ marginRight: 2 }}>Send from:</Box>{" "}
+          {/* Label on the left */}
+          <TextField
+            id="senderAcc"
+            select
+            label="Please select sending account"
+            helperText=""
+            value={transferData.senderAcc}
+            name="senderAcc"
+            sx={boxShadowStyle}
+            onChange={handleChange}
+          >
+            {accounts.map((account) => (
+              <MenuItem key={account._id} value={account._id}>
+                {account._id}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Box>
+        <Box
+          sx={{
+            width: "46ch",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ marginRight: 2 }}>Currency</Box> {/* Label on the left */}
           <TextField
             id="currency"
             select
@@ -197,7 +193,7 @@ export default function AccountTransfersPage() {
             helperText=""
             value={transferData.currency}
             name="currency"
-            sx={{width: {xs: "40%", md: "28%"} }}
+            sx={boxShadowStyle}
             onChange={handleChange}
           >
             {currencies.map((option) => (
@@ -206,13 +202,13 @@ export default function AccountTransfersPage() {
               </MenuItem>
             ))}
           </TextField>
-          <Typography sx={{ marginRight: {xs: 1, md: 2}, ml: {xs: 1, md: 2}, fontSize: {xs: "80%", md: "100%"} }}>Amt:</Typography> {/* Label on the left */}
+          <Box sx={{ marginRight: 2 }}>Amt:</Box> {/* Label on the left */}
           <TextField
             id="amt"
             label=""
             value={transferData.amount}
             name="amount"
-            sx={{width: "28%"}}
+            sx={boxShadowStyle}
             onChange={handleChangeAmt}
             error={error}
             helperText={error}
@@ -220,42 +216,38 @@ export default function AccountTransfersPage() {
         </Box>
         <Box
           sx={{
+            width: "46ch",
             display: "flex",
             alignItems: "center",
-            justifyContent: "flex-start"
           }}
         >
-          <Typography sx={{ marginRight: 3, fontSize: {xs: "80%", md: "100%"} }}>Purpose:</Typography> {/* Label on the left */}
+          <Box sx={{ marginRight: 2 }}>Purpose:</Box> {/* Label on the left */}
           <TextField
             id="Purpose"
             label="Purpose of transfer"
             variant="filled"
             name="purpose"
-            sx={{width: "71%"}}
+            sx={boxShadowStyle}
             onChange={handleChange}
-            error={fieldError}
-            helperText={fieldError}
           />
         </Box>
         <Box
           sx={{
             display: "flex",
-            justifyContent: {xs: "center", md: "flex-end"}, // Align button to the right
+            justifyContent: "flex-end", // Align button to the right
             width: "100%", // Full width
             maxWidth: "400px", // Limit the width of the main container
             mt: 3, // Add some spacing above the button
           }}
         >
           <Button
-            sx={{mr: {xs: 0, md: 3}}}
+            sx={boxShadowStyle}
             variant="contained"
             onClick={handleSubmit}
           >
             Send now
           </Button>
-          
         </Box>
-        </Paper>
       </Box>
     </>
   );
